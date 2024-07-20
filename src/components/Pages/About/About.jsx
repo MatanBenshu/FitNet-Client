f6fe3afe5e1be41b623fd059ded2b6564e7a8faa
matan <matanbsn@gmail.com>
2024-07-21 01:08:27 +0300

    adding login page register page

--- src/components/Pages/About/About.jsx
+++ src/components/Pages/About/About.jsx
@@ -1,38 +1,38 @@
-import React, { useEffect, useState } from "react";
-import "./About.css";
+import React, { useState } from 'react';
+import './About.css';
 
 const About = () => {
-  const [message, setMessage] = useState("");
-  const [content, setContent] = useState("");
+    const [message, setMessage] = useState('');
+    const [content, setContent] = useState('');
 
-  const fetchContent = async () => {
-    try {
-      const response = await fetch("http://localhost:5000/contents/about");
-      const data = await response.json();
-      if (data.success) {
-        setContent(data.user.content);
-      } else {
-        setMessage("Error fetching user data: " + data.message);
-      }
-    } catch (error) {
-      setMessage("Error fetching user data: " + error.message);
-    }
-  };
-  fetchContent();
-  return (
-    <div className="about">
-      <h2>About FitNet</h2>
-      <p>{message === "" ? content : message}</p>
-      <h3>Creators</h3>
-      <ul>
-        <li>Matan Ben Shushan</li>
-        <li>Reem Levi</li>
-        <li>Yarden Shaked</li>
-        <li>Shiraz Nagaoker</li>
-        <li>Moran Avraham</li>
-      </ul>
-    </div>
-  );
+    const fetchContent = async () => {
+        try {
+            const response = await fetch('http://localhost:5000/contents/about');
+            const data = await response.json();
+            if (data.success) {
+                setContent(data.user.content);
+            } else {
+                setMessage('Error fetching user data: ' + data.message);
+            }
+        } catch (error) {
+            setMessage('Error fetching user data: ' + error.message);
+        }
+    };
+    fetchContent();
+    return (
+        <div className="about">
+            <h2>About FitNet</h2>
+            <p>{message === '' ? content : message}</p>
+            <h3>Creators</h3>
+            <ul>
+                <li>Matan Ben Shushan</li>
+                <li>Reem Levi</li>
+                <li>Yarden Shaked</li>
+                <li>Shiraz Nagaoker</li>
+                <li>Moran Avraham</li>
+            </ul>
+        </div>
+    );
 };
 
 export default About;
--- src/components/Pages/Home/Home.jsx
+++ src/components/Pages/Home/Home.jsx
@@ -1,14 +1,12 @@
-import React from "react";
-import NavBar from "../navBar/navBar.jsx";
+import React from 'react';
 function Home() {
-  return (
-    <>
-      
-      <div className="home">
-        <h2>Welcome to FitNet</h2>
-        <p>Your fitness journey starts here!</p>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="home">
+                <h2>Welcome to FitNet</h2>
+                <p>Your fitness journey starts here!</p>
+            </div>
+        </>
+    );
 }
 export default Home;
