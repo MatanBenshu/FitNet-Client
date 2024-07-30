4b19c47134b5ed8e7a07c4e08c79c801361e3833
reamlevi <reamlevi@gmail.com>
2024-07-30 22:58:25 +0300

    fix the test file

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
 import { render, screen } from '@testing-library/react';
+import AuthProvider from './context/AuthContext.js';
 import Login from './Pages/Login/login.jsx';
+import { BrowserRouter } from 'react-router-dom';
 
 test('renders learn react link', () => {
-    render(<Login />);
-    const linkElement = screen.getByText(/Login/);
+    render(
+        <BrowserRouter>
+            <AuthProvider>
+                <Login />
+            </AuthProvider>
+        </BrowserRouter>
+    );
+    const linkElement = screen.getByText(/FitNet/);
     expect(linkElement).toBeInTheDocument();
 });
