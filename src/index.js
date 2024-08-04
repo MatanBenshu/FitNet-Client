fbab5ebeffdf3b902148d162557711b5fef1d41b
reamlevi <reamlevi@gmail.com>
2024-08-04 16:55:22 +0300

    some minor changes

--- src/index.js
+++ src/index.js
@@ -1,21 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import AuthProvider from './context/AuthContext.js';
+import {AuthContextProvider} from './context/AuthContext.js';
+import './index.css';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <React.StrictMode>
         <BrowserRouter>
-            <AuthProvider>
+            <AuthContextProvider>
                 <App />
-            </AuthProvider>
+            </AuthContextProvider>
         </BrowserRouter>
     </React.StrictMode>
 );
-
-
-reportWebVitals();
