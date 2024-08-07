fb1fca105219aa4695679bc07600d1f844fd77c8
reamlevi <reamlevi@gmail.com>
2024-08-07 03:34:23 +0300

    add tabs for liked,saved posts

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -4,16 +4,25 @@ import Share from '../share/Share';
 import './feed.css';
 import axios from 'axios';
 import { AuthContext } from '../../context/AuthContext';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import {Favorite, ThumbUp,PersonPin} from '@mui/icons-material';
+import { pink } from '@mui/material/colors';
 
 export default function Feed({ username,updateCurrentUser,updateRightBar }) {
     const [render,reRender] = useState(0);
     const [posts, setPosts] = useState([]);
     const { user } = useContext(AuthContext);
+    const [value, setValue] = useState(0);
+    console.log(value);
 
     function handleReRenderFeed ()  {
         reRender(perv =>perv +1);
         if (updateRightBar){ updateRightBar();}
        
+    };
+    function handleChange (event,newValue)  {
+        setValue(newValue);
     };
 
 
@@ -38,6 +47,19 @@ export default function Feed({ username,updateCurrentUser,updateRightBar }) {
     return (
         <div className="feed">
             <div className="feedWrapper">
+                {(username)&&
+                <div className='TabsContainer'>
+                    <Tabs
+                        value={value}
+                        onChange={handleChange}
+                        centered
+                    >
+                        <Tab icon={<PersonPin color="success" />}  iconPosition="bottom" label="Created" />
+                        <Tab  icon={<ThumbUp color="primary"/>} iconPosition="bottom" label="Liked" />
+                        <Tab icon={<Favorite sx={{ color: pink[500] }} />} iconPosition="bottom" label="Saved" />
+                    </Tabs>
+
+                </div>}
                 {(!username) && <Share handler={handleReRenderFeed}/>}
                 {posts.map((p) => (
                     <Post key={p._id} post={p} handler={handleReRenderFeed} updateCurrentUser={updateCurrentUser} />
