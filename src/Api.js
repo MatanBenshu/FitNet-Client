1a34b8a5f9cbfde21657865cba23ac8b96c66479
reamlevi <reamlevi@gmail.com>
2024-08-11 15:09:44 +0300

    some comments for the api in the file

--- src/Api.js
+++ src/Api.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
-    
+ 
+// axios instance with base url and timeout for server endpoint in env file
 const customAxios = axios.create({
     baseURL: process.env.REACT_APP_API_ENDPOINT,
     timeout: 5000,
 });
 
+// cities api for autocomplete input in event form 
 export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
 export const geoApiOptions = {
     method: 'GET',
@@ -14,7 +16,7 @@ export const geoApiOptions = {
     },
 };
 
-
+// weather api for weather component located in event pages.
 export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5/weather?';
 export const WEATHER_API_KEY = process.env.REACT_APP_OPEN_WEATHER_API_KEY;
 
